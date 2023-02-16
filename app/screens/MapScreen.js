7183158a6f2ad44fe194ebd667e968ef8f312611
Kieran Gordon <kjg2000@hw.ac.uk>
2023-02-16 20:32:20 +0000

    Add geolocation to maps

--- app/screens/MapScreen.js
+++ app/screens/MapScreen.js
@@ -1,17 +1,36 @@
+import React from "react";
 import { Button, View, Text, StyleSheet } from "react-native";
 import MapView from "react-native-maps";
+import * as Location from "expo-location";
 
-const MapScreen = () => {
+const MapScreen = (props) => {
+  const [region, setRegion] = React.useState(null);
+  React.useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.error("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
       <MapView
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
         style={styles.container}
+        initialRegion={region}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+        followsUserLocation={true}
       />
     </View>
   );
