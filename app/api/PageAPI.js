b39ade82abec70fcb6f462d9078950d352272a64
Kieran Gordon <kjg2000@hw.ac.uk>
2023-03-22 01:25:29 +0000

    Implement page deletion

--- app/api/PageAPI.js
+++ app/api/PageAPI.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import * as SecureStore from "expo-secure-store";
 
 export default class PageAPI {
   constructor(id, title, body, scrapbook, longitude, latitude) {
@@ -8,6 +9,10 @@ export default class PageAPI {
     this.scrapbook = scrapbook;
     this.longitude = longitude;
     this.latitude = latitude;
+  }
+
+  static async getToken() {
+    return await SecureStore.getItemAsync("token");
   }
 
   static async getPages() {
@@ -50,6 +55,18 @@ export default class PageAPI {
     return response.data;
   }
 
+  static async deletePage(id) {
+    const response = await axios.delete(
+      "http://94.173.211.21:8000/page/" + id + "/",
+      {
+        headers: {
+          Authorization: "Token " + (await this.getToken()),
+        },
+      }
+    );
+    return response.data;
+  }
+
   static async createComment(authorid, page, body) {
     const response = await axios.post("http://94.173.211.21:8000/comment/", {
       authorid,
@@ -57,5 +74,55 @@ export default class PageAPI {
       body,
     });
     return response.data;
+  }
+
+  static async createLike(userId, pageId) {
+    const response = await axios.post(
+      `http://94.173.211.21:8000/api/auth/likes/${pageId}/`,
+      {
+        liker: userId,
+        liked_page: pageId,
+      },
+      {
+        headers: {
+          Authorization: `Token ${await this.getToken()}`,
+        },
+      }
+    );
+    return response.data;
+  }
+
+  static async deleteLike(pageId) {
+    const response = await axios.delete(
+      `http://94.173.211.21:8000/api/auth/deletelike/${pageId}/`,
+      {
+        headers: {
+          Authorization: `Token ${await this.getToken()}`,
+        },
+      }
+    );
+    return response.data;
+  }
+
+  static async getLikeById(id) {
+    const response = await axios.get(
+      `http://94.173.211.21:8000/api/auth/likes/${id}`
+    );
+    return response.data;
+  }
+
+  static async getLikesByUser(userId) {
+    const response = await axios.get(
+      `http://94.173.211.21:8000/api/auth/userlikes/${userId}`
+    );
+    return response.data.results[0];
+  }
+
+  static async getLikesByPage(pageId) {
+    // Count the number of results in the array
+    const response = await axios.get(
+      `http://94.173.211.21:8000/api/auth/likes/${pageId}`
+    );
+    return response.data.results.length;
   }
 }
--- app/screens/PostViewScreen.js
+++ app/screens/PostViewScreen.js
@@ -7,14 +7,19 @@ import {
   TextInput,
   ToastAndroid,
   View,
+  Pressable,
+  Text,
+  Alert,
 } from "react-native";
 import { Button } from "react-native-paper";
 import AccountsAPI from "../api/AccountsAPI";
 import PageAPI from "../api/PageAPI";
+import ScrapbookAPI from "../api/ScrapbookAPI";
 import CommentsContainer from "../components/CommentsContainer";
-import LikeContainer from "../components/LikeContainer";
 import PostContainer from "../components/PostContainer";
 import LoadingContainer from "../components/LoadingContainer";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import Clipboard from "@react-native-community/clipboard";
 
 // Allow for navigation to this screen from a nested stack navigator
 export default function PostViewScreen({ route, navigation }) {
@@ -22,11 +27,10 @@ export default function PostViewScreen({ route, navigation }) {
   const [comment, setComment] = React.useState("");
   const [comments, setComments] = React.useState([]);
   const [refreshing, setRefreshing] = React.useState(false);
-  const [username, setUsername] = React.useState("");
-  const [email, setEmail] = React.useState("");
   const [id, setId] = React.useState("");
   const [likes, setLikes] = React.useState(0);
   const [liked, setLiked] = React.useState(false);
+  const [scrapbookOwner, setScrapbookOwner] = React.useState("");
 
   const fetchDetails = React.useCallback(() => {
     PageAPI.getPage(route.params.id).then((post) => {
@@ -35,42 +39,73 @@ export default function PostViewScreen({ route, navigation }) {
     PageAPI.getComments(route.params.id).then((comments) => {
       setComments(comments);
     });
-    AccountsAPI.getLikesByPage(route.params.id).then((likes) => {
+    PageAPI.getLikesByPage(route.params.id).then((likes) => {
       setLikes(likes);
     });
-    AccountsAPI.getLikesByUser(route.params.id).then((liked_page) => {
-      if (liked_page == post) {
-        setLiked(liked_page);
-      } else {
-        console.log(
-          "The user has either not liked this page or there was another error."
-        );
-      }
+    PageAPI.getLikesByUser(route.params.id).then((liked_page) => {
+      setLiked(liked_page);
     });
-  }, [route.params.id]);
-
-  // Fetch the user's account details from the API.
-  const fetchAccountDetails = React.useCallback(() => {
     AccountsAPI.getAccount().then((account) => {
-      setUsername(account.username);
-      setEmail(account.email);
       setId(account.id);
     });
-  }, []);
+    setLikes(likes);
+  }, [route.params.id, likes]);
+
+  // Add logic for liking a post and updating the like count, as well as unliking a post and updating the like count
+  const like = React.useCallback(() => {
+    if (!liked) {
+      PageAPI.createLike(id, route.params.id);
+      setLiked(true);
+      setLikes(likes + 1);
+    } else {
+      PageAPI.deleteLike(route.params.id);
+      setLiked(false);
+      setLikes(likes - 1);
+    }
+  }, [id, liked, route.params.id, fetchDetails]);
+
+  // Allow users to share a post by copying the link to the post to the clipboard.
+  const share = React.useCallback(() => {
+    Clipboard.setString(
+      "http://94.173.211.21:8000/page/" + route.params.id + "/"
+    );
+
+    ToastAndroid.show("Link copied to clipboard", ToastAndroid.SHORT);
+  }, [route.params.id]);
+
+  // If the scrapbook id matches a scrapbook owned by the user, allow the user to delete the post.
+  const deletePost = React.useCallback(() => {
+    Alert.alert(
+      "Delete Post",
+      "Are you sure you want to delete this post?",
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Delete",
+          onPress: () => {
+            PageAPI.deletePage(route.params.id);
+            navigation.goBack();
+          },
+        },
+      ],
+      { cancelable: false }
+    );
+  }, [route.params.id, navigation]);
 
   // When the page is focused, fetch the post and comments from the API.
   React.useEffect(() => {
     fetchDetails();
-    fetchAccountDetails();
-  }, [fetchDetails, fetchAccountDetails]);
+  }, [fetchDetails]);
 
   // When the user pulls down to refresh, fetch the post and comments from the API.
   const onRefresh = React.useCallback(() => {
     setRefreshing(true);
     fetchDetails();
-    fetchAccountDetails();
     setRefreshing(false);
-  }, [fetchDetails, fetchAccountDetails]);
+  }, [fetchDetails]);
 
   // Show a loading indicator while the post and comments are being fetched.
   if (post === null) {
@@ -97,12 +132,49 @@ export default function PostViewScreen({ route, navigation }) {
           />
         )}
         <SafeAreaView style={styles.container}>
-          <LikeContainer
-            user_id={id}
-            post_id={post.id}
-            liked={liked}
-            likes={likes}
-          />
+          <View style={styles.likeContainer}>
+            <Pressable onPress={like}>
+              {/* if liked_page with id exists, show filled heart, else show empty heart */}
+              {liked ? (
+                <MaterialIcons
+                  name="favorite"
+                  size={30}
+                  color="red"
+                  style={{ marginLeft: 10 }}
+                />
+              ) : (
+                <MaterialIcons
+                  name="favorite-border"
+                  size={30}
+                  color="black"
+                  style={{ marginLeft: 10 }}
+                />
+              )}
+            </Pressable>
+            <Text style={styles.likes}>{likes}</Text>
+            <Pressable onPress={share}>
+              <MaterialIcons
+                name="share"
+                size={30}
+                color="black"
+                style={{ marginLeft: 10 }}
+              />
+            </Pressable>
+            {/* If post belongs to user, display delete button */}
+            <Pressable
+              onPress={() => {
+                deletePost();
+              }}
+            >
+              <MaterialIcons
+                name="delete"
+                size={30}
+                color="black"
+                // display on right side of screen
+                style={{ marginLeft: 10 }}
+              />
+            </Pressable>
+          </View>
           {/* add button to text field to submit commment */}
           <TextInput
             style={styles.commentBox}
@@ -174,5 +246,25 @@ const styles = StyleSheet.create({
     margin: 5,
     padding: 5,
     backgroundColor: "#e96b37",
+  },
+  likeContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    borderRadius: 10,
+    backgroundColor: "#fff",
+  },
+  likes: {
+    marginLeft: 5,
+  },
+  like: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "center",
+    marginBottom: 5,
   },
 });
